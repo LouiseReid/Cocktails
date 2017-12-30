42b54f906e0cfaf8402f487f2c05f97e21782d0a
LouiseReid <l_reid@hotmail.co.uk>
2017-12-30 10:11:14 +0000

    replaceWith kind of working

--- public/app.js
+++ public/app.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
 
-  $('.ingredients-button').on('click', () => {
+  $('.ingredients-button').on('click', function(){
     $('.ingredients').toggle();
   });
+
+  // $('.cocktail-image').hover( function (){
+  //   $('.cocktail-image').replace('.cocktail-name')
+  // })
+
+  $('.cocktail').on('click', function(){
+    $('.cocktail-image').replaceWith('cocktail-name')
+  })
+
 
 });
