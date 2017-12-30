2a9d964866229fe7d0cf8b0aca0153e3c8733edd
LouiseReid <l_reid@hotmail.co.uk>
2017-12-30 14:46:15 +0000

    ingredients only show for button clicked

--- public/app.js
+++ public/app.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
 
   $('.ingredients-button').on('click', function(){
-    $('.ingredients').toggle();
+    var clickedButton = $(this).next()
+    clickedButton.toggle()
   });
 
 
-  $(".cocktail").hover(function () {
-    $(".cocktail-image").slideUp('slow')
-    $('.cocktail-name').show()
-  }, function () {
-    $(".cocktail-image").slideDown('slow')
-    $('.cocktail-name').hide()
 
-  });
+
+  // $('.cocktail').hover(function () {
+  //   $('.cocktail-image').slideUp('slow')
+  //   $('.cocktail-name').show()
+  // }, function () {
+  //   $('.cocktail-image').slideDown('slow')
+  //   $('.cocktail-name').hide()
+  // });
 
 
 });
