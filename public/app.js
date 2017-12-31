93b5e2ec0525f8903220c5b7777f20ba25ce49a3
LouiseReid <l_reid@hotmail.co.uk>
2017-12-31 11:45:19 +0000

    rendering functions added

--- public/app.js
+++ public/app.js
@@ -12,8 +12,38 @@ $(document).ready(function(){
   }).mouseout(function (e) {
     $(this).attr("src", $(this).attr("src").replace(".png", ".jpeg"));
     $(".ingredients-button").show()
-
   });
 
+  for(var cocktail of cocktailsArray){
+    addCocktail(cocktail.name, cocktail.ingredients)
+  }
 
 });
+
+ var cocktailsArray = [
+  {name: "mojito", ingredients:["lime", "rum", "sugar"]}
+]
+
+var createObject = function(){
+  $("<div></div>").addClass("cocktail").appendTo('#cocktail-section')
+
+}
+
+var createImage = function(name){
+  var img = $("<img>").addClass("cocktail-image").appendTo("cocktail");
+  img.attr('src', name)
+}
+
+var createIngredients = function(ingredients){
+  $("<div></div>").addClass("ingredients").appendTo("cocktail")
+  var cList = $("ul.cList")
+  $.each(ingredients, function(i){
+    var li = $("<li/>").text(ingredients[i]).appendTo(cList)
+  })
+}
+
+var addCocktail = function(picture, ingredients){
+  createObject();
+  createImage(picture);
+  createIngredients(ingredients);
+}
