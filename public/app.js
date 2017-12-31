ae34f78b16c764ed1bd9c8ca6ca316840dc44e1d
LouiseReid <l_reid@hotmail.co.uk>
2017-12-31 14:54:33 +0000

    cocktails rendering from array

--- public/app.js
+++ public/app.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 
+console.log(cocktailsArray);
+  $.each(cocktailsArray, function(key, cocktail){
+    addCocktail(cocktail.name, cocktail.ingredients)
+  })
+
   $(".ingredients-button").on("click", function(){
+    console.log($(this).next());
     var ingredients = $(this).next()
     ingredients.toggle()
   });
@@ -14,28 +20,26 @@ $(document).ready(function(){
     $(".ingredients-button").show()
   });
 
-  for(var cocktail of cocktailsArray){
-    addCocktail(cocktail.name, cocktail.ingredients)
-  }
-
 });
 
  var cocktailsArray = [
-  {name: "mojito", ingredients:["lime", "rum", "sugar"]}
+  {name: "mojito.jpeg", ingredients:["lime", "rum", "sugar"]},
+  {name: "pinacolada.jpeg", ingredients:["coconut liquer", "rum", "pineapple juice"]},
+  {name: "tomcollins.jpeg", ingredients:["lemon", "gin", "sugar"]}
 ]
 
 var createObject = function(){
-  $("<div></div>").addClass("cocktail").appendTo('#cocktail-section')
-
+  $("<div></div>").addClass("cocktail").appendTo('main')
 }
 
 var createImage = function(name){
-  var img = $("<img>").addClass("cocktail-image").appendTo("cocktail");
+  var img = $("<img>").addClass("cocktail-image").appendTo(".cocktail");
   img.attr('src', name)
+  $("<button/>").text("Ingredients").addClass("ingredients-button").appendTo(".cocktail")
 }
 
 var createIngredients = function(ingredients){
-  $("<div></div>").addClass("ingredients").appendTo("cocktail")
+  $("<div></div>").addClass("ingredients").appendTo(".cocktail")
   var cList = $("ul.cList")
   $.each(ingredients, function(i){
     var li = $("<li/>").text(ingredients[i]).appendTo(cList)
